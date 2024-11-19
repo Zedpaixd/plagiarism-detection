40d792133ceeba845e6798b1179b50a09848d97b
zedpaixd <planta636@gmail.com>
2024-11-20 01:17:35 +0200

    reminder for folder creation

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from filesimilarity import FileSimilarity
 
 
 if __name__ == '__main__':
+
+    if not os.path.exists('submissions'):
+        os.makedirs('submissions')
+        print("Created submissions folder. Please put all .zip and .rar submissions in there.")
+        exit(0)
+
     # fileprops = FileProperties(os.path.join(os.getcwd(), 'submissions'))
     # fileprops.copy_paste_plagiarism()
 
